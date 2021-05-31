ac3942883cba298435bc2a658166472c65e5567b
plegoffdev <85097829+plegoffdev@users.noreply.github.com>
2021-05-31 16:14:40 +0200

    Update Greeter.java
    Add javadoc to constructor and method greet()

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,12 +2,21 @@ package com.example;
 
 
 public class Greeter {
-
   
+  /**
+  *default constructor
+  */
   public Greeter() {
 
   }
 
+    /**
+* Return a string, etc... 
+*
+* 
+* @param  String someone
+* @return      String
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
